823b5c4d186559878ef4d5ceac0c7ddcf9a6080d
ZHOUGR2 <286348794@qq.com>
2020-07-23 19:57:38 +0800

    TDD [Green] feature:implement GuessNumberGame guess function when answer is 1234 given 1623.

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,23 +1,66 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuessNumberGame {
-    private  int countTimes = 0;
+    private int countTimes = 0;
     private String answer;
-    public GuessNumberGame(){
+    private List<String> answerSplitList = new ArrayList<String>();
+    private List<String> guessSplitList = new ArrayList<String>();
+
+    public GuessNumberGame() {
 
     }
-    public GuessNumberGame(String answer){
+
+    public GuessNumberGame(String answer) {
         this.answer = answer;
+        String splitAnswer[] = answer.split("");
+        for (String s : splitAnswer) {
+            answerSplitList.add(s);
+        }
     }
-    public GuessNumberGame(AnswerGenerate answerGenerate){
+
+    public GuessNumberGame(AnswerGenerate answerGenerate) {
         this.answer = answerGenerate.generate();
     }
-    public String guess(String guess){
-        if (answer.equals(guess)){
-            return "4A0B";
-        }else {
-            return "2A2B";
-        }
 
+    public String guess(String guess) {
+        String splitGuess[] = guess.split("");
+        for (String s : splitGuess) {
+            guessSplitList.add(s);
+        }
+        String locationRight = locationRightAndNumberRight();
+        String numberRight = locationErrorButNumberRight();
+        return locationRight + numberRight;
     }
+
+    public String locationRightAndNumberRight() {
+        int coutLocationRightAndNumberRight = 0;
+        for (int i = 0; i < guessSplitList.size(); i++) {
+            if (islocationRight(guessSplitList.get(i), i)) {
+                coutLocationRightAndNumberRight++;
+            }
+        }
+        return coutLocationRightAndNumberRight + "A";
+    }
+
+    public String locationErrorButNumberRight() {
+        int coutlocationErrorButNumberRight = 0;
+        for (int i = 0; i < guessSplitList.size(); i++) {
+            if (!islocationRight(guessSplitList.get(i), i) && isNumberRight(guessSplitList.get(i))) {
+                coutlocationErrorButNumberRight++;
+            }
+        }
+        return coutlocationErrorButNumberRight + "B";
+    }
+
+    public boolean islocationRight(String s, int location) {
+        return answerSplitList.get(location).equals(s);
+    }
+
+    public boolean isNumberRight(String num) {
+        return answerSplitList.contains(num);
+    }
+
 }
