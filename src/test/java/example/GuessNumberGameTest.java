5342f7ea38b56127a1876581f967108e12eb36e5
ZHOUGR2 <286348794@qq.com>
2020-07-23 18:56:33 +0800

    TDD [Green] test:add should_return_2A2B_when_guess_1234_given_1243 test case.

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -13,6 +13,17 @@ public class GuessNumberGameTest {
         //when
         String actual = guessNumberGame.guess(guessNumber);
         //then
-        Assertions.assertEquals(answer,actual);
+        Assertions.assertEquals("4A0B",actual);
+    }
+    @Test
+    public  void  should_return_2A2B_when_guess_1234_given_1243(){
+        //given
+        String answer = "1234";
+        String guessNumber = "1243";
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        //when
+        String actual = guessNumberGame.guess(guessNumber);
+        //then
+        Assertions.assertEquals("2A2B",actual);
     }
 }
