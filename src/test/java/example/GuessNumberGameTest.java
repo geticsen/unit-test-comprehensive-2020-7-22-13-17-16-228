b218895fcab3e68668313dfc850bef904b0fff23
ZHOUGR2 <286348794@qq.com>
2020-07-23 21:17:14 +0800

    TDD [Green] test:add should_return_wrong_message_when_guess_1234_given_1111 test case.

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class GuessNumberGameTest {
     @Test
-    public  void  should_return_4A0B_when_guess_1234_given_1234(){
+    public void should_return_4A0B_when_guess_1234_given_1234() {
         //given
         String answer = "1234";
         String guessNumber = "1234";
@@ -13,10 +13,11 @@ public class GuessNumberGameTest {
         //when
         String actual = guessNumberGame.guess(guessNumber);
         //then
-        Assertions.assertEquals("4A0B",actual);
+        Assertions.assertEquals("4A0B", actual);
     }
+
     @Test
-    public  void  should_return_2A2B_when_guess_1234_given_1243(){
+    public void should_return_2A2B_when_guess_1234_given_1243() {
         //given
         String answer = "1234";
         String guessNumber = "1243";
@@ -24,10 +25,11 @@ public class GuessNumberGameTest {
         //when
         String actual = guessNumberGame.guess(guessNumber);
         //then
-        Assertions.assertEquals("2A2B",actual);
+        Assertions.assertEquals("2A2B", actual);
     }
+
     @Test
-    public  void  should_return_1A2B_when_guess_1234_given_1623(){
+    public void should_return_1A2B_when_guess_1234_given_1623() {
         //given
         String answer = "1234";
         String guessNumber = "1623";
@@ -35,10 +37,11 @@ public class GuessNumberGameTest {
         //when
         String actual = guessNumberGame.guess(guessNumber);
         //then
-        Assertions.assertEquals("1A2B",actual);
+        Assertions.assertEquals("1A2B", actual);
     }
+
     @Test
-    public  void  should_return_0A4B_when_guess_1234_given_4321(){
+    public void should_return_0A4B_when_guess_1234_given_4321() {
         //given
         String answer = "1234";
         String guessNumber = "4321";
@@ -46,10 +49,11 @@ public class GuessNumberGameTest {
         //when
         String actual = guessNumberGame.guess(guessNumber);
         //then
-        Assertions.assertEquals("0A4B",actual);
+        Assertions.assertEquals("0A4B", actual);
     }
+
     @Test
-    public  void  should_return_0A1B_when_guess_1234_given_5178(){
+    public void should_return_0A1B_when_guess_1234_given_5178() {
         //given
         String answer = "1234";
         String guessNumber = "5178";
@@ -57,10 +61,11 @@ public class GuessNumberGameTest {
         //when
         String actual = guessNumberGame.guess(guessNumber);
         //then
-        Assertions.assertEquals("0A1B",actual);
+        Assertions.assertEquals("0A1B", actual);
     }
+
     @Test
-    public  void  should_return_0A0B_when_guess_1234_given_5678(){
+    public void should_return_0A0B_when_guess_1234_given_5678() {
         //given
         String answer = "1234";
         String guessNumber = "5678";
@@ -68,6 +73,17 @@ public class GuessNumberGameTest {
         //when
         String actual = guessNumberGame.guess(guessNumber);
         //then
-        Assertions.assertEquals("0A0B",actual);
+        Assertions.assertEquals("0A0B", actual);
+    }
+    @Test
+    public void should_return_wrong_message_when_guess_1234_given_1111() {
+        //given
+        String answer = "1234";
+        String guessNumber = "1111";
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        //when
+        String actual = guessNumberGame.play(guessNumber);
+        //then
+        Assertions.assertEquals("Wrong Input,Input again", actual);
     }
 }
