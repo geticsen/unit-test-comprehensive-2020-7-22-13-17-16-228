975fb7b5e6b24cdb4343cee35dcdba7dc815db45
geticsen <286348794@qq.com>
2020-07-27 21:17:54 +0800

    refactor [Green] refactor :rename noRepeatNumber to  noRepeatNumber

--- src/main/java/example/AnswerGenerateImp.java
+++ src/main/java/example/AnswerGenerateImp.java
@@ -4,14 +4,14 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class AnswerGenerateImp implements AnswerGenerate{
-    private List<Integer> noRepeatNumber = new LinkedList<Integer>();
+    private List<Integer> noRepeatNumbers = new LinkedList<Integer>();
     @Override
     public String generate() {
         String answer = "";
-        while(noRepeatNumber.size()<4){
+        while(noRepeatNumbers.size()<4){
             int randomInt = ((int)(Math.random()*10))%10;
-            if(!noRepeatNumber.contains(randomInt)){
-                noRepeatNumber.add(randomInt);
+            if(!noRepeatNumbers.contains(randomInt)){
+                noRepeatNumbers.add(randomInt);
                 answer +=randomInt;
             }
         }
--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -102,8 +102,8 @@ public class GuessNumberGame {
         return answerSplitList.contains(num);
     }
     public static void main(String []args) throws IOException {
-        InputStreamReader is = new InputStreamReader(System.in); //new构造InputStreamReader对象
-        BufferedReader br = new BufferedReader(is); //拿构造的方法传到BufferedReader中
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
 
         GuessNumberGame guessNumberGame = new GuessNumberGame(new AnswerGenerateImp().generate());
         String answer = guessNumberGame.getAnswer();
