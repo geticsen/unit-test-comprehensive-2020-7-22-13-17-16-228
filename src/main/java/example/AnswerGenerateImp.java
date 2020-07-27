6e6a0577c6fa97e0bf84a5ed70cbaaed7ecd5903
geticsen <286348794@qq.com>
2020-07-27 21:21:33 +0800

    refactor [Green] refactor : Format code

--- src/main/java/example/AnswerGenerateImp.java
+++ src/main/java/example/AnswerGenerateImp.java
@@ -3,21 +3,23 @@ package example;
 import java.util.LinkedList;
 import java.util.List;
 
-public class AnswerGenerateImp implements AnswerGenerate{
+public class AnswerGenerateImp implements AnswerGenerate {
     private List<Integer> noRepeatNumbers = new LinkedList<Integer>();
+
     @Override
     public String generate() {
         String answer = "";
-        while(noRepeatNumbers.size()<4){
-            int randomInt = ((int)(Math.random()*10))%10;
-            if(!noRepeatNumbers.contains(randomInt)){
+        while (noRepeatNumbers.size() < 4) {
+            int randomInt = ((int) (Math.random() * 10)) % 10;
+            if (!noRepeatNumbers.contains(randomInt)) {
                 noRepeatNumbers.add(randomInt);
-                answer +=randomInt;
+                answer += randomInt;
             }
         }
         return answer;
     }
-    public static void  main(String[]args){
+
+    public static void main(String[] args) {
         AnswerGenerateImp answerGenerateImp = new AnswerGenerateImp();
         System.out.println(answerGenerateImp.generate());
     }
